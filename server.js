efe9bc6608e45cab080117796a809831c524ccd3
Djuslun <djuslun@yandex.ru>
2023-04-24 23:01:17 +0300

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const server = jsonServer.create()
 
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('db/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(cors());
